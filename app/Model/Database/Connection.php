af7ba478a30792161a111471023dfb7825aae7ed
Harshana Wijesinghe <harshawije99@gmail.com>
2023-11-08 21:30:10 +0530

    refactoring

--- app/Model/Database/Connection.php
+++ app/Model/Database/Connection.php
@@ -1,5 +1,33 @@
-<?php namespace database;
+<?php
 
-final class Connection {
-    
-} 
+namespace App\Model\Database;
+
+use PDO;
+
+
+final class Connection
+{
+
+    protected mixed $connection;
+
+    private function __construct()
+    {
+    }
+
+    protected function setPDOConnection()
+    {
+        try {
+            $conn_string = new PDO("sqlite:" . __DIR__ . '/app/Model/Database/app.sqlite');
+            $conn_string->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $this->connection = $conn_string;
+        } catch (\Throwable $error) {
+            return $error->__toString();
+        }
+    }
+
+    protected function getPDOConnection()
+    {
+        return $this->connection;
+    }
+}
--- index.php
+++ index.php
@@ -3,3 +3,5 @@
 require __DIR__ . '/vendor/autoload.php';
 
 require __DIR__ . '/app/UI/views/home.view.php';
+
+
