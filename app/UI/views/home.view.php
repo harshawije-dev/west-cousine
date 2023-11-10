f5dc12b1e1f20946cc8f3d484184e75e60c572c0
Harshana Wijesinghe <harshawije99@gmail.com>
2023-11-10 20:10:28 +0530

    layout refactoring

--- app/UI/views/home.view.php
+++ app/UI/views/home.view.php
@@ -1,15 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Home Page</title>
-    <link rel="stylesheet" href="/west-cousine/dist/output.css" />
-  </head>
 
-  <body>
-    <div class="flex flex-row justify-center w-full items-center">
-      <h2 class="text-2xl font-medium text-gray-700">Hello World</h2>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Home Page</title>
+  <link rel="stylesheet" href="/dist/output.css" />
+</head>
+
+<body>
+  <div class="flex flex-row justify-center pb-10 w-full items-center">
+    <h2 class="text-4xl font-bold italic text-gray-700">Signature Cousine</h2>
+  </div>
+
+  <div class="flex w-auto mx-12 h-screen flex-row border-[1px] border-slate-200 rounded-md">
+    <div class="w-1/2 px-5 border-x-[0.5px]">
+      <div class="flex flex-col justify-start items-center w-full py-14">
+        <form action="">
+          <div class="mb-6">
+            <input type="text" placeholder="Add Item Name" class="form-control">
+          </div>
+          <div class="mb-6">
+            <input type="text" placeholder="Serve Mins." class="form-control">
+          </div>
+          <div class="mb-6">
+            <input type="number" placeholder="Price" class="form-control">
+          </div>
+          <div class="mt-12">
+            <input type="submit" value="Add Item" class="bg-green-400 ring-1 ring-green-500 px-10 py-3.5 rounded-md cursor-pointer font-normal text-slate-700">
+          </div>
+        </form>
+      </div>
     </div>
-  </body>
+    <div class="w-1/2 px-5"></div>
+  </div>
+</body>
+
 </html>
