b3dc5b71f1f5b707453c283093f35863f8277ba3
Harshana Wijesinghe <harshawije99@gmail.com>
2023-11-11 00:25:52 +0530

    template create

--- app/UI/views/home.view.php
+++ app/UI/views/home.view.php
@@ -33,14 +33,27 @@
       </div>
     </div>
     <div class="w-1/2 px-5">
-      <?php
-
-      use App\DataAccess\FoodsDTO\FoodItemOperation;
-
-      $db = new FoodItemOperation();
-      $data = $db->introFunction();
-      var_dump($data);
-      ?>
+      <table>
+        <thead>
+          <tr>
+            <td>Item Name</td>
+            <td>Waiting</td>
+            <td>Price</td>
+            <td>Ratings</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+        <tfoot></tfoot>
+      </table>
     </div>
   </div>
 </body>
